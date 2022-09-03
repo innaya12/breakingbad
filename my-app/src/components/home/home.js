438672b9acb5ea4c98974b1af7c81e613f32fa85
innaya12 <yakubov.inna2@gmail.com>
2022-09-03 18:23:51 +0300

    main page displays in home page

---
+++ my-app/src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 id="title">Breaking Bad</h1>
+        </div>
+    )
+};
+
+export default Home;
